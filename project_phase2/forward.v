7e67394ddd8112ab7ec1001f0e50d5791f618b25
jchen123556 <jchen123556@gmail.com>
2019-11-14 19:14:48 -0600

    fixed size of ports

--- project_phase2/forward.v
+++ project_phase2/forward.v
@@ -1,15 +1,15 @@
-module ForwardingUnit(input [4:0] IDEX_rs, input [4:0] IDEX_rt, input [4:0] EXMEM_rd, input EXMEM_rw, 
-	input MEMWB_rw, input [4:0] MEMWB_rd, input [4:0] EXMEM_rs, input [4:0] EXMEM_rt, 
+module ForwardingUnit(input [3:0] IDEX_rs, input [3:0] IDEX_rt, input [3:0] EXMEM_rd, input EXMEM_rw, 
+	input MEMWB_rw, input [3:0] MEMWB_rd, input [3:0] EXMEM_rs, input [3:0] EXMEM_rt, 
 	output [1:0] ex_fwd1, output [1:0] ex_fwd2, output mem_fwd1, output mem_fwd2);
 	
 	wire [1:0] extoex1, extoex2;
 	
-	assign extoex1 = (EXMEM_rw && (EXMEM_rd != 5'b00000) && (EXMEM_rd == IDEX_rs)) ? 2'b10 : 2'b00;
-	assign extoex2 = (EXMEM_rw && (EXMEM_rd != 5'b00000) && (EXMEM_rd == IDEX_rt)) ? 2'b10 : 2'b00;
+	assign extoex1 = (EXMEM_rw && (EXMEM_rd != 4'b0000) && (EXMEM_rd == IDEX_rs)) ? 2'b10 : 2'b00;
+	assign extoex2 = (EXMEM_rw && (EXMEM_rd != 4'b0000) && (EXMEM_rd == IDEX_rt)) ? 2'b10 : 2'b00;
 	
-	assign ex_fwd1 = (MEMWB_rw && (MEMWB_rd != 5'b00000) && ~(EXMEM_rw && (EXMEM_rd != 5'b00000) && (EXMEM_rd != IDEX_rs)) && (MEMWB_rd == IDEX_rs)) ? 2'b01 : extoex1;
-	assign ex_fwd2 = (MEMWB_rw && (MEMWB_rd != 5'b00000) && ~(EXMEM_rw && (EXMEM_rd != 5'b00000) && (EXMEM_rd != IDEX_rt)) && (MEMWB_rd == IDEX_rt)) ? 2'b01 : extoex2;
+	assign ex_fwd1 = (MEMWB_rw && (MEMWB_rd != 4'b0000) && ~(EXMEM_rw && (EXMEM_rd != 4'b0000) && (EXMEM_rd != IDEX_rs)) && (MEMWB_rd == IDEX_rs)) ? 2'b01 : extoex1;
+	assign ex_fwd2 = (MEMWB_rw && (MEMWB_rd != 4'b0000) && ~(EXMEM_rw && (EXMEM_rd != 4'b0000) && (EXMEM_rd != IDEX_rt)) && (MEMWB_rd == IDEX_rt)) ? 2'b01 : extoex2;
 	
-	assign memfwd1 = (MEMWB_rw && (MEMWB_rd != 5'b00000) && (MEMWB_rd == EXMEM_rs)) ? 1'b1 : 1'b0;
-	assign memfwd2 = (MEMWB_rw && (MEMWB_rd != 5'b00000) && (MEMWB_rd == EXMEM_rt)) ? 1'b1 : 1'b0;
+	assign memfwd1 = (MEMWB_rw && (MEMWB_rd != 4'b0000) && (MEMWB_rd == EXMEM_rs)) ? 1'b1 : 1'b0;
+	assign memfwd2 = (MEMWB_rw && (MEMWB_rd != 4'b0000) && (MEMWB_rd == EXMEM_rt)) ? 1'b1 : 1'b0;
 endmodule
